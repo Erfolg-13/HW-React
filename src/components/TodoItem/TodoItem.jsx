f5eff1bb382d9de3ba509855956c305f683483be
Erfolg-13 <erfolg_13@ukr.net>
2021-04-03 12:27:43 +0300

    change function to change status onClick Button and added Css

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,27 +1,29 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import './TodoItem.css';
 
-function TodoItem (props) {
-    const handleChangeStatus = useCallback(() => {
-        props.onChange(props.status, props.name)
-    }, [props.status, props.name])
-    // const [status, setStatus] = useState(props.status);
+function TodoItem(props) {
+    
+    // const handleChangeStatus = useCallback(() => {
+    //     props.onChange(props.status, props.name)
+    // }, [props.status, props.name])
 
-    // The first example - not used:
-    // const handleChangeStatus = useCallback (() => {
-    //     if (status === "new") {
-    //         setStatus("progress");
-    //     } else if (status === "progress") {
-    //         setStatus("done");
-    //     }else if (status === "done") {
-    //         setStatus("new");
-    //     }
-    // }, [status])
+    const [status, setStatus] = useState('new');
+
+    const handleChangeStatus = useCallback (() => {
+        if (status === "new") {
+            return setStatus("progress");
+        } else if (status === "progress") {
+            return setStatus("done");
+        } else if (status === "done") {
+            return setStatus("new");
+        };
+        props.onChange(status);
+    }, [status, props]);
 
     return (
-        <div className="todo-item">
-            <div className={`todo-item_status is-${props.status}`}>
-                {props.status}
+        <div className={`todo-item is-${status}`}>
+            <div className={`todo-item_status is-${status}`}>
+                {status}
             </div>
             <div className="todo-item_name">
                 {props.name}
